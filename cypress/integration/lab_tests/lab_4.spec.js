9b954576a49185acb02d3de1c65d58be62b9d931
Alex Leitch <aleitch1@umd.edu>
2020-09-15 16:27:37 -0400

    Update lab 4 spec to be more complete

--- cypress/integration/lab_tests/lab_4.spec.js
+++ cypress/integration/lab_tests/lab_4.spec.js
@@ -7,8 +7,53 @@ describe('Lab 4', () => {
     });
   });
 
+  it('should contain your name and lab number within the page title', () => {
+    cy.fixture('test_values').then((json) => {
+      cy.title().then(($title) => {
+        const [name, lab, title] = [json.name, 'lab 4', $title].map((m) => m.toUpperCase());
+        expect(title.includes(name)).to.be.true;
+        expect(title.includes(lab)).to.be.true;
+      });
+    });
+  });
+
+  it('should correctly link to supplied lab styles.css', () => {
+    cy.get('head link[href="styles.css"]');
+  });
+
+  it('should contain a div, class wrapper, which contains a div, class container, which holds the rest of your visible HTML', () => {
+    cy.get('.wrapper .container');
+  });
+
+  it('Your .container div should include a div, class header, containing a headline with your name and the lab number in it', () => {
+    cy.fixture('test_values').then((json) => {
+      cy.get('.header h1')
+        .then(($hh1) => {
+          const [name, lab, title] = [json.name, 'lab 4', $hh1.text()].map((m) => m.toUpperCase());
+          expect(title.includes(name)).to.be.true;
+          expect(title.includes(lab)).to.be.true;
+        });
+    });
+  });
+
   it('Should include an HTML form', () => {
     cy.get('form');
+  });
+
+  it('Should include an input field for text, with an id', () => {
+    cy.get('form input[type=text]')
+      .then(($ta) => {
+        const id = $ta.attr('id');
+        expect(id).to.exist;
+      });
+  });
+
+  it('Your text input should have a <label> with correct for attribute', () => {
+    cy.get('form input[type=text]')
+      .then(($txt) => {
+        const id = $txt.attr('id');
+        cy.get(`form label[for=${id}]`);
+      });
   });
 
   it('Should include a Textarea field that has 5 rows and 33 columns', () => {
@@ -22,15 +67,37 @@ describe('Lab 4', () => {
       });
   });
 
-  it('Should have an input field for text, with an attached label element', () => {
+  it('Your Textarea field should have an id attribute', () => {
+    cy.get('form textarea')
+      .then(($ta) => {
+        const id = $ta.attr('id');
+        expect(id).to.exist;
+      });
+  });
+
+  it('Your Textarea should have a <label> with correct for attribute', () => {
+    cy.get('form textarea')
+      .then(($txt) => {
+        const id = $txt.attr('id');
+        cy.get(`form label[for=${id}]`); // broken because getting only first label
+      });
+  });
+
+  it('All form controls should have a name attribute, for identification of data on server', () => {
+    cy.get('form textarea')
+      .then(($txt) => {
+        const name = $txt.attr('name');
+        expect(name).to.exist;
+      });
     cy.get('form input[type=text]')
       .then(($txt) => {
-        const name = $txt.attr('id');
-        cy.get('form label')
-          .then(($lbl) => {
-            const fr = $lbl.attr('for');
-            expect(fr).equals(name);
-          });
+        const name = $txt.attr('name');
+        expect(name).to.exist;
+      });
+    cy.get('form button')
+      .then(($txt) => {
+        const name = $txt.attr('name');
+        expect(name).to.exist;
       });
   });
 
@@ -38,15 +105,13 @@ describe('Lab 4', () => {
     cy.get('form').should('have.attr', 'method', 'post');
     cy.get('button[type=submit]')
       .click();
-    // TODO: MAKE SURE THIS CHECKS A NEW PAGE
-    // TODO: ASSIGNMENT 1 SPEC
     cy.contains('hello world', { matchCase: false });
   });
 
   it('Should receive an HTML element containing "Hello World" from the server', () => {
     cy.fixture('test_values').then((json) => {
       const labUrl = `${json.test_context || ''}/lab_4/`;
-      cy.visit(labUrl); // change URL to match your dev URL
+      cy.visit(labUrl);
       cy.get('button[type=submit]')
         .click();
       cy.contains('hello world', { matchCase: false });
@@ -54,6 +119,6 @@ describe('Lab 4', () => {
   });
 
   it('Should link to the lab CSS file and be styled appropriately', () => {
-    cy.get('body')
+    cy.get('body');
   });
 });
