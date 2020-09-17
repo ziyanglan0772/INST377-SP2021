9384ca1fde0bc9a9a86c4e38c2be464070cb6762
Alex Leitch <aleitch1@umd.edu>
2020-09-17 09:00:11 -0400

    Tweak server post request message

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.route('/api')
   })
   .post((req, res) => {
     console.log('POST request detected');
-    console.log('Request body', req.body);
+    console.log('Form data in res.body', req.body);
   });
 
 app.listen(port, () => {
