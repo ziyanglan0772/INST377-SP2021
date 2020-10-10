f3705127c3f35f966308e8c177ac09d490417377
Alex Leitch <aleitch1@umd.edu>
2020-10-09 22:07:02 -0400

    Add some pocket functions for util files

--- public/lab_6/script.js
+++ public/lab_6/script.js
@@ -1,3 +1,17 @@
+// You may wish to find an effective randomizer function on MDN.
+
+function range(int) {
+  const arr = [];
+  for (let i = 0; i < int; i += 1) {
+    arr.push(i);
+  }
+  return arr;
+}
+
+function sortFunction(a, b, key) {
+  return (a[key] < b[key] ? -1 : (a[key] > b[key] ? 1 : 0));
+}
+
 document.body.addEventListener('submit', async (e) => {
   e.preventDefault(); // this stops whatever the browser wanted to do itself.
   const form = $(e.target).serializeArray(); // here we're using jQuery to serialize the form
@@ -11,7 +25,7 @@ document.body.addEventListener('submit', async (e) => {
     .then((fromServer) => fromServer.json())
     .then((fromServer) => {
       // You're going to do your lab work in here. Replace this comment.
-      console.log(fromServer);
+      console.log('fromServer', fromServer);
     })
     .catch((err) => console.log(err));
 });
