130ed9a742fa126a9b2b86946a77c64366ab33a7
Alex Leitch <aleitch1@umd.edu>
2020-10-09 20:43:15 -0400

    Add initial Lab 6 support files

---
+++ public/lab_6/script.js
@@ -0,0 +1,17 @@
+document.body.addEventListener('submit', async (e) => {
+  e.preventDefault(); // this stops whatever the browser wanted to do itself.
+  const form = $(e.target).serializeArray(); // here we're using jQuery to serialize the form
+  fetch('/api', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(form)
+  })
+    .then((fromServer) => fromServer.json())
+    .then((fromServer) => {
+      // You're going to do your lab work in here. Replace this comment.
+      console.log(fromServer);
+    })
+    .catch((err) => console.log(err));
+});
