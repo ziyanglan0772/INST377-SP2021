0bdd45a6b173985048fcc063046c11afcbccbbbe
Alex Leitch <aleitch1@umd.edu>
2020-10-26 10:09:35 -0400

    Further split functions to support unit testing in cypress

--- public/lab_7/script.js
+++ public/lab_7/script.js
@@ -1,11 +1,19 @@
-function convertRestaurantsToCategories(restaurantList){
+function convertRestaurantsToCategories(restaurantList) {
   // process your restaurants here!
   return list;
 }
 
+function makeYourOptionsObject(datapointsFromRestaurantsList) {
+  // set your chart configuration here!
+  return canvasJSConfigObject;
+} 
+
 function runThisWithResultsFromServer(jsonFromServer) {
-  // This should become a list of restaurants by category
   console.log('jsonFromServer', jsonFromServer);
+  sessionStorage.setItem('restaurantList', JSON.stringify(jsonFromServer)); // don't mess with this, we need it to provide unit testing support
+  // Process your restaurants list
+  // Make a configuration object for your chart
+  // Instantiate your chart
 }
 
 document.body.addEventListener('submit', async (e) => {
