2c61591a6cd5d5a82e9819c14a6e8f36fd34f514
Alex Leitch <aleitch1@umd.edu>
2020-10-26 09:50:15 -0400

    Tweak script so Cypress can do unit testing support

--- public/lab_7/script.js
+++ public/lab_7/script.js
@@ -1,3 +1,13 @@
+function convertRestaurantsToCategories(restaurantList){
+  // process your restaurants here!
+  return list;
+}
+
+function runThisWithResultsFromServer(jsonFromServer) {
+  // This should become a list of restaurants by category
+  console.log('jsonFromServer', jsonFromServer);
+}
+
 document.body.addEventListener('submit', async (e) => {
   e.preventDefault(); // this stops whatever the browser wanted to do itself.
   const form = $(e.target).serializeArray();
@@ -9,10 +19,7 @@ document.body.addEventListener('submit', async (e) => {
     body: JSON.stringify(form)
   })
     .then((fromServer) => fromServer.json())
-    .then((jsonFromServer) => {
-      // This should become a list of restaurants by category
-      console.log('jsonFromServer', jsonFromServer);
-    })
+    .then((jsonFromServer) => runThisWithResultsFromServer(jsonFromServer))
     .catch((err) => {
       console.log(err);
     });
