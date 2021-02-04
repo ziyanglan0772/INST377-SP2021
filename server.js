0970074d997a0c4b8015c027cde877495e86c8da
Alex Leitch <aleitch1@umd.edu>
2021-02-04 09:09:49 -0500

    Add new server files

--- server.js
+++ server.js
@@ -4,9 +4,23 @@ import path from 'path';
 import logger from 'morgan';
 import dotenv from 'dotenv';
 import chalk from 'chalk';
+import Sequelize from 'sequelize';
 import fetch from 'node-fetch';
 
+const env = process.env.NODE_ENV || 'development';
+const db = {};
+
 dotenv.config();
+
+const ormSql = new Sequelize({
+  username: 'student',
+  password: 'INST377@UMD',
+  host: '3.236.243.212',
+  dialect: 'mysql',
+  ssl: 'Amazon RDS',
+  pool: { maxConnections: 5, maxIdleTime: 30},
+  language: 'en'
+});
 
 const app = express();
 const staticFolder = 'public';
@@ -26,6 +40,13 @@ app.use((req, res, next) => {
 });
 
 app.listen(port, async () => {
+  try {
+    await ormSql.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+
   console.log(`Webpage available at localhost:${port}`);
   console.log(chalk.bold.magenta(`Hot reload available at localhost:${port + 1}`));
   browserSync({
@@ -34,6 +55,7 @@ app.listen(port, async () => {
     open: false,
     port: port + 1,
     proxy: `localhost:${port}`,
-    ui: false
+    ui: false,
+    notify: false
   });
 });
---
+++ server/router.js
@@ -0,0 +1,13 @@
+app.route('/api')
+  .get(async (req, res) => {
+    console.log('GET request detected');
+    const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+    const json = await data.json();
+    console.log('data from fetch', json);
+    res.json(json);
+  })
+  .post(async (req, res) => {
+    console.log('POST request detected');
+    console.log('Form data in res.body', req.body);
+    res.json({facilities: dataStore});
+  });
