166b4e04a2cc8622fec84314045fdb8d897e2e34
Alex Leitch <aleitch1@umd.edu>
2021-02-04 09:09:41 -0500

    Rearrange lab dependencies

--- public/labs/lab_6/script.js
+++ public/labs/lab_6/script.js
@@ -1,26 +1,56 @@
-// You may wish to find an effective randomizer function on MDN.
-
-function range(int) {
-  const arr = [];
-  for (let i = 0; i < int; i += 1) {
-    arr.push(i);
-  }
-  return arr;
+function convertRestaurantsToCategories(restaurantList) {
+  // process your restaurants here!
+  return list;
 }
 
-function sortFunction(org, comparison, key) {
-  if (org[key] < comparison[key]) {
-    return -1;
-  } if (org[key] > comparison[key]) {
-    return 1;
-  }
-  return 0;
+function makeYourOptionsObject(datapointsFromRestaurantsList) {
+  // set your chart configuration here!
+  CanvasJS.addColorSet('customColorSet1', [
+    // add an array of colors here https://canvasjs.com/docs/charts/chart-options/colorset/
+  ]);
+
+  return {
+    animationEnabled: true,
+    colorSet: 'customColorSet1',
+    title: {
+      text: 'Change This Title'
+    },
+    axisX: {
+      interval: 1,
+      labelFontSize: 12
+    },
+    axisY2: {
+      interlacedColor: 'rgba(1,77,101,.2)',
+      gridColor: 'rgba(1,77,101,.1)',
+      title: 'Change This Title',
+      labelFontSize: 12,
+      scaleBreaks: {customBreaks: []} // Add your scale breaks here https://canvasjs.com/docs/charts/chart-options/axisy/scale-breaks/custom-breaks/
+    },
+    data: [{
+      type: 'bar',
+      name: 'restaurants',
+      axisYType: 'secondary',
+      dataPoints: datapointsFromRestaurantsList
+    }]
+  };
 }
 
+function runThisWithResultsFromServer(jsonFromServer) {
+  console.log('jsonFromServer', jsonFromServer);
+  sessionStorage.setItem('restaurantList', JSON.stringify(jsonFromServer)); // don't mess with this, we need it to provide unit testing support
+  // Process your restaurants list
+  // Make a configuration object for your chart
+  // Instantiate your chart
+  const reorganizedData = convertRestaurantsToCategories(jsonFromServer);
+  const options = makeYourOptionsObject(reorganizedData);
+  const chart = new CanvasJS.Chart('chartContainer', options);
+  chart.render();
+}
+
+// Leave lines 52-67 alone; do your work in the functions above
 document.body.addEventListener('submit', async (e) => {
   e.preventDefault(); // this stops whatever the browser wanted to do itself.
-  const form = $(e.target).serializeArray(); // here we're using jQuery to serialize the form
-  // set fave to yes
+  const form = $(e.target).serializeArray();
   fetch('/api', {
     method: 'POST',
     headers: {
@@ -29,14 +59,8 @@ document.body.addEventListener('submit', async (e) => {
     body: JSON.stringify(form)
   })
     .then((fromServer) => fromServer.json())
-    .then((jsonFromServer) => {
-      // You're going to do your lab work in here. Replace this comment.
-      
-      console.log('jsonFromServer', jsonFromServer);
-      const reverseList = newArr2.sort((a, b) => sortFunction(b, a, 'name'));
-    })
+    .then((jsonFromServer) => runThisWithResultsFromServer(jsonFromServer))
     .catch((err) => {
-      console.log(err)
-      // set fave to no
+      console.log(err);
     });
 });
