f075c475fb351e0f4120cb1937c9e89c8cffbc13
ziyanglan0772 <63257147+ziyanglan0772@users.noreply.github.com>
2021-05-02 21:18:47 +0800

    111

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,24 @@
-/* Put your javascript in here */
+
+   	let slider = document.querySelector('#slider');
+    let width = 150; 
+    let count = 3;
+
+    let list = slider.querySelector('ul');
+    let lis = slider.querySelectorAll('li');
+
+    let pos = 0;
+    let prev = slider.querySelector('.prev');
+    let next = slider.querySelector('.next');
+    // left
+    prev.onclick = function() {
+      pos = pos + width * count;
+      pos = Math.min(pos, 0);
+      list.style.marginLeft = pos + 'px';
+    };
+    // right
+    next.onclick = function() {
+      pos = pos - width * count;
+      pos = Math.max(pos, -width * (lis.length - count));
+      list.style.marginLeft = pos + 'px';
+    };
+  
