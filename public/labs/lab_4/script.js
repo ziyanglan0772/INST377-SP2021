394552b801b8d915eeaf23eae01aa1e00c6466e7
ziyanglan0772 <63257147+ziyanglan0772@users.noreply.github.com>
2021-05-10 22:59:48 +0800

    update script

--- public/labs/lab_4/script.js
+++ public/labs/lab_4/script.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/api', (req, res) => {
+app.get('/lab5/api', (req, res) => {
   res.send('Hello World!')
+})
+app.listen(port, () => {
+  console.log(`Lab 4 app listening at http://localhost:${port}`)
 })
