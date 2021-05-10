074e0a02c7388aa794a07ade9b8b21f81b7c42d9
ziyanglan0772 <63257147+ziyanglan0772@users.noreply.github.com>
2021-05-10 23:00:00 +0800

    ss

--- public/labs/lab_4/script.js
+++ public/labs/lab_4/script.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/lab5/api', (req, res) => {
+app.get('/api', (req, res) => {
   res.send('Hello World!')
 })
 app.listen(port, () => {
