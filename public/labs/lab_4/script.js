869d36da1a65777c5e07e9d5b7df17f54ec88be9
ziyanglan0772 <63257147+ziyanglan0772@users.noreply.github.com>
2021-05-10 22:45:29 +0800

    change script

--- public/labs/lab_4/script.js
+++ public/labs/lab_4/script.js
@@ -1,10 +1,24 @@
 
-var express = require("express");
-var app = express();
-var router = express.Router()
+   	let slider = document.querySelector('#slider');
+    let width = 150; 
+    let count = 3;
 
-router.use('/api', function(req, res) {
-  res.send('Hello Word');
-})
+    let list = slider.querySelector('ul');
+    let lis = slider.querySelectorAll('li');
 
-app.listen(3000);
+    let pos = 0;
+    let prev = slider.querySelector('.prev');
+    let next = slider.querySelector('.next');
+    // left
+    prev.onclick = function() {
+      pos = pos + width * count;
+      pos = Math.min(pos, 0);
+      list.style.marginLeft = pos + 'px';
+    };
+    // right
+    next.onclick = function() {
+      pos = pos - width * count;
+      pos = Math.max(pos, -width * (lis.length - count));
+      list.style.marginLeft = pos + 'px';
+    };
+  
